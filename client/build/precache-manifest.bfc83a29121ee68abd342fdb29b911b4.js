093ee75e6594d9fe64b380a4663705807dbb3e5d
Andrew Higton <andrewhigton@outlook.com>
2021-02-26 21:05:43 +0000

    sass-install

---
+++ client/build/precache-manifest.bfc83a29121ee68abd342fdb29b911b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b62f679e814089175818b638d33d2560",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5b3e467c8d5f7125672",
+    "url": "/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "d69c93d3968704d07d88",
+    "url": "/static/css/main.e3077cfb.chunk.css"
+  },
+  {
+    "revision": "d5b3e467c8d5f7125672",
+    "url": "/static/js/2.02672314.chunk.js"
+  },
+  {
+    "revision": "e035e169d6e31a84ba19d710491c246d",
+    "url": "/static/js/2.02672314.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d69c93d3968704d07d88",
+    "url": "/static/js/main.ba1ddf62.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
